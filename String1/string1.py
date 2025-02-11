91664a96fc7dc1c62c77d9526614bd26d8280a9d
jinnyujinchoi <bblinging22@gmail.com>
2025-02-11 11:29:57 +0900

    add_string

---
+++ String1/string1.py
@@ -0,0 +1,2 @@
+print(f'{ord("대"):x}')  # b300
+print(chr(0xb300))      # 대
---
+++ String1/string_test.py
@@ -0,0 +1,12 @@
+s1 = list(input())
+print(s1)
+s2 = input()
+print(s2)
+print(s1[1])
+print(s2[1])
+s1[1] = 'e'
+print(s1)
+# s2[1] = 'e' 오류 납니다!!!!!
+print(s2)
+s2 = 'aec'
+print(s2)
