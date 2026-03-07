a5519bc8d56869cbb4917d6cfe254ad24a31ad5d
Jin <bblinging22@gmail.com>
2026-03-07 13:33:29 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -12,15 +12,11 @@ d = N - start # 더할 값
 # 시작점 찾고, 분수 찾기
 if row%2 == 0:
     x, y = 1, row
-else:
-    x, y = row, 1
-
-if d == 0:
-    pass
-elif row%2 == 0: # 행이 짝수라면,
     x += d
     y -= d
 else:
+    x, y = row, 1
     x -= d
     y += d
+
 print(f'{x}/{y}')
